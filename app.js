5163aa2cb102ae8564555b7f1a4ca3edb8ce887f
darkimpow <darkimpowell@gmail.com>
2022-12-22 10:13:07 -0500

    added audio file for testing keydown fucntion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener('keydown', function(e) {
-    const audio= document.querySelector(`audio[data-key="${e.keycode}"]`);
-    const key= document.querySelector(`.key[data-key="${e.keycode}"]`);
+    const audio= document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key= document.querySelector(`.key[data-key="${e.keyCode}"]`);
    if(!audio) return; // stop the audio function from running all together
    audio.currentTime=  0; // rewinds the sound to start point
    audio.play();
@@ -9,6 +9,7 @@ window.addEventListener('keydown', function(e) {
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return; // skip if its not a transform event happening 
+    
     this.classlist.remove('playing');
 }
 
